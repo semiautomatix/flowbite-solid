b323d300892e31eb513182e0c59edc160e5f4659
Tim <tim@gaul.dev>
2024-03-13 12:42:02 +0000

    Working with vitest

--- src/components/Alert/Alert.test.tsx
+++ src/components/Alert/Alert.test.tsx
@@ -1,37 +1,39 @@
-import { render, cleanup, fireEvent, screen } from 'solid-testing-library';
-import { jest } from '@jest/globals';
-import Alert from './Alert';
+import { describe, it, expect, afterEach, vi } from "vitest";
+import { render, cleanup, fireEvent, screen } from "@solidjs/testing-library";
+import { Alert } from "./Alert";
 
-describe('Alert Component', () => {
+describe("Alert Component", () => {
   afterEach(cleanup);
 
-  test('renders without crashing', () => {
+  it("renders without crashing", () => {
     render(() => <Alert />);
-    expect(screen.getByRole('alert')).toBeInTheDocument();
+    expect(screen.getByRole("alert")).toBeInTheDocument();
   });
 
-  test('displays custom class when provided', () => {
-    const customClass = 'my-custom-class';
+  it("displays custom class when provided", () => {
+    const customClass = "my-custom-class";
     render(() => <Alert class={customClass} />);
-    expect(screen.getByRole('alert')).toHaveClass(customClass);
+    expect(screen.getByRole("alert")).toHaveClass(customClass);
   });
 
-  test('renders additional content when provided', () => {
-    const additionalContent = <div data-testid="additional-content">More Info</div>;
+  it("renders additional content when provided", () => {
+    const additionalContent = (
+      <div data-testid="additional-content">More Info</div>
+    );
     render(() => <Alert additionalContent={additionalContent} />);
-    expect(screen.getByTestId('additional-content')).toBeInTheDocument();
+    expect(screen.getByTestId("additional-content")).toBeInTheDocument();
   });
 
-  test('icon is displayed when provided', () => {
+  it("icon is displayed when provided", () => {
     const Icon = () => <svg data-testid="custom-icon"></svg>;
     render(() => <Alert icon={Icon} />);
-    expect(screen.getByTestId('custom-icon')).toBeInTheDocument();
+    expect(screen.getByTestId("custom-icon")).toBeInTheDocument();
   });
 
-  test('calls onDismiss when the dismiss button is clicked', () => {
-    const onDismissMock = jest.fn();
+  it("calls onDismiss when the dismiss button is clicked", () => {
+    const onDismissMock = vi.fn();
     render(() => <Alert onDismiss={onDismissMock} />);
-    fireEvent.click(screen.getByLabelText('Dismiss'));
+    fireEvent.click(screen.getByLabelText("Dismiss"));
     expect(onDismissMock).toHaveBeenCalled();
   });
 });
